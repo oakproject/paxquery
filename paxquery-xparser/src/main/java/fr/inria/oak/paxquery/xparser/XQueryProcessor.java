855bbaf57c221b98c9ef15f6ffef04dfe26369a7
jamnaranjo <juan.alvaro-munoz.naranjo@inria.fr>
2014-04-09 12:01:43 +0200

    Folder restructuring for maven standards compliance

--- paxquery-xparser/src/main/java/fr/inria/oak/paxquery/xparser/XQueryProcessor.java
+++ paxquery-xparser/src/main/java/fr/inria/oak/paxquery/xparser/XQueryProcessor.java
@@ -4,8 +4,32 @@ import fr.inria.oak.paxquery.common.xml.treepattern.core.*;
 
 import java.util.HashMap;
 
-import org.antlr.v4.runtime.misc.NotNull;
-
 public class XQueryProcessor extends XQueryBaseListener {
+	public HashMap<String, PatternNode> patternNodeMap;
+	public TreePattern treePattern;
+	private String lastPatternNodeRef = "";
+	
+	public XQueryProcessor() {
+		patternNodeMap = new HashMap<String, PatternNode>();
+		treePattern = new TreePattern();
+	}
+
+	public void enterPathExprInner_xq_VAR(XQueryParser.PathExprInner_xq_VARContext ctx) { 
+/*		System.out.println("Found VAR");
+		PatternNode rootNode = new PatternNode("", "", -1, "", "");
+		rootNode.setTag(ctx.getChild(0).getText());
+		lastPatternNodeRef = ctx.getChild(0).getText();
+		patternNodeMap.put(rootNode.getTag(), rootNode);*/
+	}
+
+	
+	public void enterXpath ( XQueryParser.XpathContext ctx ) {
+//		System.out.println("Entering Xpath path at " + ctx.parent.getText());		
+	}
+	
+	public void enterNameTest_qName ( XQueryParser.NameTest_qNameContext ctx ) {
+		//nodes.add(ctx.qName().getText());
+		
+	}
 
 }
