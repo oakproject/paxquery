f0d5588fba4d9aa8774b871e9a0204592e10f12e
Juan Álvaro <juanalvaro83@gmail.com>
2014-10-20 22:16:57 +0200

    Scripts update for installation and execution

--- paxquery-xparser/src/main/scripts/paxquery-run.sh
+++ paxquery-xparser/src/main/scripts/paxquery-run.sh
@@ -3,15 +3,22 @@
 ##########
 ## Configuration by the user
 ### set FLINK_PATH to the path to Flink directory in your system
-FLINK_PATH=
+FLINK_PATH="/Users/jalvaro/flink-0.6-incubating"
+#FLINK_PATH=
 ### set PAXQUERY_INSTALLATION_PATH  to the path where Paxquery is installed
-PAXQUERY_INSTALLATION_PATH=
+PAXQUERY_INSTALLATION_PATH="/Users/jalvaro/paxquery-installation"
+#PAXQUERY_INSTALLATION_PATH=
 ##########
 
 # Arguments check
 if [ $# -lt 2 ]; then
-	echo "Usage: paxquery-run.sh [query_file] [output_file]"
+	echo "Usage: paxquery-run.sh [query_file] [output_file] <paralellism> <drawtrees>"
 	exit 1
+fi
+
+drawtrees=0
+if [ $# -eq 4 ] && [ $4 == "drawtrees" ]; then
+	drawtrees=1
 fi
 
 # Paths check
@@ -32,9 +39,19 @@ PARALELLISM=$3
 
 PAXQUERY_INSTALLATION_BIN=$PAXQUERY_INSTALLATION_PATH"/bin"
 PAXQUERY_INSTALLATION_LIB=$PAXQUERY_INSTALLATION_PATH"/lib"
-export CC_CLASSPATH=$PAXQUERY_INSTALLATION_BIN"/paxquery-algebra-0.1.jar:"$PAXQUERY_INSTALLATION_BIN"/paxquery-translation-0.1.jar:"$PAXQUERY_INSTALLATION_LIB"/antlr-runtime-4.2.jar"
+#export CC_CLASSPATH=$PAXQUERY_INSTALLATION_BIN"/paxquery-algebra-0.1.jar:"$PAXQUERY_INSTALLATION_BIN"/paxquery-translation-0.1.jar:"$PAXQUERY_INSTALLATION_LIB"/antlr-runtime-4.2.jar"
+export CC_CLASSPATH=$PAXQUERY_INSTALLATION_BIN"/paxquery-algebra-0.1.jar:"$PAXQUERY_INSTALLATION_BIN"/paxquery-translation-0.1.jar:"$PAXQUERY_INSTALLATION_LIB"/antlr-runtime-4.2.jar:"$PAXQUERY_INSTALLATION_LIB"/json-simple-1.1.jar"
 XPARSER_JAR=$PAXQUERY_INSTALLATION_BIN"/paxquery-xparser-0.1.jar"
+WEB_PATH=$PAXQUERY_INSTALLATION_PATH/web/images/plans
+
+echo ""
+echo "-- This is PAXQuery 0.1 --"
+echo ""
 
 #Actions
 rm $2 &> /dev/null	#hide rm output from the user
-"$FLINK_PATH"/bin/flink run -v $XPARSER_JAR file://"$QUERY_FILE" file://"$OUTPUT_DIR"/"$OUTPUT_FILE" $3
+if [ $drawtrees -eq 0 ]; then
+	"$FLINK_PATH"/bin/flink run -v $XPARSER_JAR file://"$QUERY_FILE" file://"$OUTPUT_DIR"/"$OUTPUT_FILE" $3
+else
+	"$FLINK_PATH"/bin/flink run -v $XPARSER_JAR file://"$QUERY_FILE" file://"$OUTPUT_DIR"/"$OUTPUT_FILE" $3 drawtrees $WEB_PATH
+fi
